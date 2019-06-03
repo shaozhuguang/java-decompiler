cc2a929980647e8df7a4c9f7cbabcfd4a0847d6b
shaozhuguang <shaozhuguang@jd.com>
2019-06-03 18:07:27 +0800

    初始化代码

---
+++ src/main/java/com/farmer/x/samples/PrintTools.java
@@ -0,0 +1,8 @@
+package com.farmer.x.samples;
+
+public class PrintTools {
+
+    public void print(String name, int age) {
+        System.out.printf("My Name is %s and Age is %s \r\n", name, age);
+    }
+}
